718c3ae18bf92d7091f0f6583e722e2c7ce3441f
Admin <admin@Admins-MacBook-Pro.local>
2020-04-09 10:28:28 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Iron_Ninja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
